5e4abdd1341edcf9b171c4e43534e077e1efe027
Sebastian Dolber <sdolber@icloud.com>
2014-02-14 12:05:30 -0500

    initial layout

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,12 @@
 // Put your application scripts here
+$(document).ready(function() {
+    var text_max = 500;
+    $('#dropit_count').html(text_max);
+
+    $('#dropit_input').keyup(function() {
+        var text_length = $('#dropit_input').val().length;
+        var text_remaining = text_max - text_length;
+
+        $('#dropit_count').html(text_remaining);
+    });
+});
