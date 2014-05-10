82a26eb352d702ba2e258782fca11af532d4a346
jsdolber <sdolber@gmail.com>
2014-05-09 21:26:15 -0400

    increase pool size

--- config/database.rb
+++ config/database.rb
@@ -1,4 +1,4 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
+MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger, :pool_size => 5, :timeout => 5)
 
 case Padrino.env
   when :development then MongoMapper.database = 'tiralabomba_development'
