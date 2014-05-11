864e1566354f5894cefdd43b5e5c92874fd54b41
jsdolber <sdolber@gmail.com>
2014-05-11 10:40:39 -0400

    mongo mierda

--- config/database.rb
+++ config/database.rb
@@ -1,4 +1,5 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger, :pool_size => 5, :timeout => 5)
+MongoMapper.connection = Mongo::MongoClient.new("localhost", nil, :logger => logger, :pool_size => 10, :pool_timeout => 30)
+#Mongo::Connection.new('localhost', nil, :logger => logger, :pool_size => 5, :query_timeout => 30)
 
 case Padrino.env
   when :development then MongoMapper.database = 'tiralabomba_development'
