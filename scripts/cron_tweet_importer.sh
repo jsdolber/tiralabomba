c0e9db5b45ae920601ad947bdefbc39c5291da9e
jsdolber <sdolber@gmail.com>
2014-09-13 20:45:06 -0400

    import tweets script

--- scripts/cron_tweet_importer.sh
+++ scripts/cron_tweet_importer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-importer_path=`/var/www/tiralabomba.com/public_html/padrino runner -e production /var/www/tiralabomba.com/public_html/runners/import_tweets.rb`
-eval importer_path
+cd /var/www/tiralabomba.com/public_html/
+padrino runner -e production runners/import_tweets.rb
